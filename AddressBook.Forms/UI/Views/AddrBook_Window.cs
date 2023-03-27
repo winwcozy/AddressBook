88e37f6264c6391d7a4984034dbfce2080bbcd3b
jsw <jowhq700@gmail.com>
2023-03-28 00:31:29 +0900

    First Commit

---
+++ AddressBook.Forms/UI/Views/AddrBook_Window.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace AddressBook.Forms.UI.Views
+{
+    public class AddrBook_Window : Window
+    {
+        static AddrBook_Window()
+        {
+            DefaultStyleKeyProperty.OverrideMetadata(typeof(AddrBook_Window), new FrameworkPropertyMetadata(typeof(AddrBook_Window)));
+        }
+    }
+}
