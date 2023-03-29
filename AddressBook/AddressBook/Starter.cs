5417ac5ea074885813d4944455334552a085a06e
JSW <jowhq700@gmail.com>
2023-03-29 09:57:05 +0900

    test commit

---
+++ AddressBook/AddressBook/Starter.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace AddressBook
+{
+    internal class Starter
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="args"></param>
+        [STAThread]
+        private static void Main(string[] args)
+        {
+            new App().Run(); 
+        }
+    }
+}
